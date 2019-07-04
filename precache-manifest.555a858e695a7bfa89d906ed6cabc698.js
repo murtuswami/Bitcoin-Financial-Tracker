d85c2dd3088583285ad2ee950d9d170247e3cf93
murtuswami <harsharamachandran14@gmail.com>
2019-07-04 14:47:57 +0800

    Updates

---
+++ precache-manifest.555a858e695a7bfa89d906ed6cabc698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1d45097abea48172d5cee7bc638076",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c18f6fd15f5067a080b3",
+    "url": "/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "d30397ccd29edbd4ed04",
+    "url": "/static/css/main.c406093c.chunk.css"
+  },
+  {
+    "revision": "c18f6fd15f5067a080b3",
+    "url": "/static/js/2.a2476f1f.chunk.js"
+  },
+  {
+    "revision": "d30397ccd29edbd4ed04",
+    "url": "/static/js/main.d5840ffe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
